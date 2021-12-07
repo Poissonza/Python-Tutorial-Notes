13134f7970061a93600f064e1b978db83af838d2
Adam Borlase <adam.borlase@proquoai.com>
2021-12-08 00:12:29 +0200

    Added in Chapter 1 for Journal club summary (Bayes)

---
+++ data/Think Bayes/Chapter 1/get_gss_data.py
@@ -0,0 +1,12 @@
+# Load the data file
+
+from os.path import basename, exists
+
+def download(url):
+    filename = basename(url)
+    if not exists(filename):
+        from urllib.request import urlretrieve
+        local, _ = urlretrieve(url, filename)
+        print('Downloaded ' + local)
+
+download('https://github.com/AllenDowney/BiteSizeBayes/raw/master/gss_bayes.csv')
